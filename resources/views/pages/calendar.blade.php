2d4aedb95a7e77895d3831682773df0144f68f3e
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2021-12-07 16:17:54 +0800

    tried to integrate stuff

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -1,60 +1,14 @@
 <!-- test of the calendar widget -->
 
-<!DOCTYPE html>
-<html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.app')
 
-        <title>Argon Dashboard</title>
+@section('content')
 
-        <!-- Favicon -->
-<link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
-
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-
-<!-- Icons -->
-<link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.min.css" rel="stylesheet">
-<link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
-
-<!-- Argon CSS -->
-<link type="text/css" href="{{ asset('argon') }}/css/argon.min.css" rel="stylesheet">
-
-<!-- Calendar Stuff -->
-
-<link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.css' rel='stylesheet' />
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.js'></script>
-
-<script>
-
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
-
-</script>
-    </head>
-
-    <body>
         <h1>Hello, World</h1>
 
         <div id='calendar'></div>
 
-
-        <!-- Core -->
-<script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
-<script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- Argon JS -->
-<script src="{{ asset('argon') }}/js/argon.min.js"></script>
-    </body>
-
-</html>
 
 
 
