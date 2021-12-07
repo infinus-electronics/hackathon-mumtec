7a95c91162caf0b2191e71d97a92c604ab7627a4
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2021-12-07 08:56:02 +0000

    fix vendor

---
+++ storage/framework/views/239a635f62dbf5843466a005527b77cc14b66de6.php
@@ -0,0 +1,21 @@
+<!-- test of the calendar widget -->
+
+
+
+
+<?php $__env->startSection('content'); ?>
+<div class = "main-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+
+                <div id='calendar'></div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/pages/calendar.blade.php ENDPATH**/ ?>
