d068bd7465f17f0b5e2826ded44aa53b4902609f
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2021-12-07 16:23:11 +0800

    fix

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -5,15 +5,15 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
 
-            <div id='calendar'></div>
+                <div id='calendar'></div>
 
+            </div>
         </div>
     </div>
-</div>
 
 
 
