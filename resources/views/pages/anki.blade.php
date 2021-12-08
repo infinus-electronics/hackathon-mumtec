85a021c4aba64758ca92b1e48f9a17c5de352ddd
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2021-12-08 16:39:05 +0800

    fix typo

--- resources/views/pages/anki.blade.php
+++ resources/views/pages/anki.blade.php
@@ -19,7 +19,7 @@
                 <div class = "card text-center">
                     <h3 class = "card-header">Today's practice question</h3>
                     <img class = "card-img-top pb-2" src="https://source.unsplash.com/sECcwm6BN8w/1920x1080" alt="Image Placeholder"> 
-                    <p class="card-text">This is just a random practice questioon.</p>
+                    <p class="card-text">This is just a random practice question.</p>
                     <a href="#" class="btn btn-primary">View answer</a>
                     <div class="card-footer text-muted">Last revised m days ago</div>
                 </div>            
