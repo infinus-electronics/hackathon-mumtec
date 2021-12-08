4323fdd67551d2103620b1fb62183b3956ab22d0
woonyee28 <ngnwy289@gmail.com>
2021-12-08 13:54:43 +0800

    chatbot-blade

--- resources/views/pages/chatbot.blade.php
+++ resources/views/pages/chatbot.blade.php
@@ -3,4 +3,17 @@
 @section('content')
     @include('layouts.headers.app-head')
     
+    <H1 class="text-center">CLICK THE MAIL ICON TO CHAT WITH ME</H1>
+    <img style="display: block;margin-left: auto;margin-right: auto;width: 50%;position:absolute;" src="https://i.pinimg.com/736x/02/b1/3c/02b13cd6e197d1456c42792a71b7cb75.jpg" alt="Picture" class="center">
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+    <script>
+        var botmanWidget = {
+            aboutText: 'ssdsd',
+            introMessage: "✋ Hi! I'm Yoong Ken, yes, that guy. :) Thanks for reaching out! How may I help you? Science, History, or how to bancuh kopi?"
+        };
+    </script>
+    
+    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+    
 @endsection
